6976fecfa16136498a8bfe5e31ee84677f1e61fb
Marty Powell <jpowel25@gmail.com>
2017-06-29 08:48:55 -0400

    added interval timer logic

---
+++ bc.interval-timer.js
@@ -0,0 +1,63 @@
+var IntervalTimer = (function () {
+  return function (callback, interval) {
+    var timer;
+    var startTime;
+    var TIMER_STATES = {
+      "0": "idle",
+      "1": "running",
+      "2": "paused",
+      "3": "resumed"
+    };
+
+    this.interval = interval;
+    this.callback = callback;
+    this.state = 0;
+    this.remainingTimeInInterval = 0;
+
+    var start = function () {
+      startTime = new Date();
+      timer = setInterval(callback, interval);
+      this.state = 1;
+    }.bind(this);
+
+    var timeoutCallback = function () {
+      if (this.GetState() !== "resumed") {
+        return;
+      }
+      callback();
+
+      startTime = new Date();
+      timer = window.setInterval(callback, interval);
+      this.state = 1;
+    }.bind(this);
+
+    this.GetState = function () {
+      return TIMER_STATES[this.state.toString()];
+    }.bind(this);
+
+    this.Pause = function () {
+      if (this.GetState() === "paused") {
+        return;
+      }
+
+      this.remainingTimeInInterval = interval - (new Date() - startTime);
+      this.Stop();
+      this.state = 2;
+    }.bind(this);
+
+    this.Resume = function () {
+      if (this.GetState() !== "paused") {
+        return;
+      }
+
+      this.state = 3;
+      window.setTimeout(timeoutCallback, this.remainingTimeInInterval);
+    }.bind(this);
+
+    this.Stop = function () {
+      clearInterval(timer);
+    }.bind(this);
+
+    start();
+  }
+})();
