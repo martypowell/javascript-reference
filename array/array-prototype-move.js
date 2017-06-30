89f1d73fe7bdc3aa3f560b5fca6ec40a789e49bf
Marty Powell <jpowel25@gmail.com>
2017-06-30 11:58:58 -0400

    Updated to allow for negative values ot be passed in, to tack on the end of the array

--- array/array-prototype-move.js
+++ array/array-prototype-move.js
@@ -1,12 +1,27 @@
-Array.prototype.move = function (old_index, new_index) {
-    var newIndexIsTooLarge = new_index >= this.length;
-    if (newIndexIsTooLarge) {
-      console.error('The new index is greater than the length of the array');
-    }
-    if (old_index === new_index || newIndexIsTooLarge) {
-      return this;
-    }
-
-    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
-    return this; // for testing purposes
-};
+(function () {
+  if (!Array.prototype.move) {
+    Array.prototype.move = function (old_index, new_index) {
+      var newIndexIsTooLarge = new_index >= this.length;
+      if (newIndexIsTooLarge) {
+        console.error('The new index is greater than the length of the array');
+      }
+      if (this.length === 0 || newIndexIsTooLarge) {
+        return this;
+      }
+      while (old_index < 0) {
+        old_index += this.length;
+      }
+      while (new_index < 0) {
+        new_index += this.length;
+      }
+      if (new_index >= this.length) {
+        var k = new_index - this.length;
+        while ((k--) + 1) {
+          this.push(undefined);
+        }
+      }
+      this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+      return this; // for testing purposes
+    };
+  }
+})(this);
